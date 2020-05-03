876caff0346fd7bf0f2b483f90b54a1168499fc2
Ana <anatdona@gmail.com>
2020-05-03 01:22:44 -0400

    Creo que estamos

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,10 @@ $(function(){
 
 
       $('[data-toggle="popover"]').popover();
-      
+
+      $('.popover-dismiss').popover({
+        trigger: 'focus'
+      })      
 
     $('[data-toggle="tooltip"]').tooltip();
     
