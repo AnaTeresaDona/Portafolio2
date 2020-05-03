675ecd24fe62f3d8ca3d2dd64d48b38929757db6
Ana <anatdona@gmail.com>
2020-05-03 04:05:26 -0400

    Arreglo de falla de JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
 $(function(){
     
 
-    $("a").click(function(event){
-        if (this.hash !== "") {
-          event.preventDefault();
-    
-          var gato = this.hash;
-    
-          $("html, body").animate({
-            scrollTop: $(gato).offset().top
-          }, 800, function(){
-            window.location.hash = gato;
-          });
-        }
-      });
-    
-      $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-      })
+  $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  
+    $('#myModal').on('shown.bs.modal', function () {
+      $('#myInput').trigger('focus')
+    })
 
 
-      $('[data-toggle="popover"]').popover();
-
-      $('.popover-dismiss').popover({
-        trigger: 'focus'
-      })      
+    $('[data-toggle="popover"]').popover();
 
     $('[data-toggle="tooltip"]').tooltip();
-    
+  
 
 
 });
